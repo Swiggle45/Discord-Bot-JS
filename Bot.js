1dbfbe4e3a83d0cf2259d9846f0aa794493b4fec
Swiggle45 <jpswingle3@me.com>
2021-12-17 00:33:51 -0500

    new commands and events structure

--- Bot.js
+++ Bot.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const { token } = require('./config.json');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, Intents, Interaction } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -14,77 +15,84 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
-    console.log('Ready!');
-})
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// console.log(client);
+
 
 /* ------------------------------------------------------------------*/
 
-function roller(message) {
-    const errorMessage = "syntax: \'roll dn\' where n is the highest number on the dice\nExample: roll d20";
-    const start = message.author.toString() + " You rolled: ";
-    let m = message.content;
-    if (RegExp("roll d").test(m) == false) {
-        return errorMessage;
-    }
-    m = m.replace('roll d', '');
-    if (m.length < 1) {
-        return errorMessage;
-    }
-    if (RegExp("[^0-9]").test(m)) {
-        let rand = Math.floor(Math.random() * m.length);
-        return start + m.substring(rand, rand+1);
-    }
-    let answer = Math.floor(Math.random() * parseInt(m)) + 1;
-    return start + answer;
-}
+// function roller(message) {
+//     const errorMessage = "syntax: \'roll dn\' where n is the highest number on the dice\nExample: roll d20";
+//     const start = message.author.toString() + " You rolled: ";
+//     let m = message.content;
+//     if (RegExp("roll d").test(m) == false) {
+//         return errorMessage;
+//     }
+//     m = m.replace('roll d', '');
+//     if (m.length < 1) {
+//         return errorMessage;
+//     }
+//     if (RegExp("[^0-9]").test(m)) {
+//         let rand = Math.floor(Math.random() * m.length);
+//         return start + m.substring(rand, rand+1);
+//     }
+//     let answer = Math.floor(Math.random() * parseInt(m)) + 1;
+//     return start + answer;
+// }
 
 
-let staticReply = new Map();
-staticReply['ping'] = 'Pong!';
-staticReply['hi'] = 'Hi!';
-staticReply['hello'] = 'Hello!';
-staticReply['lefty help'] = '';
-staticReply['test'] = client;
-//console.log(staticReply);
+// let staticReply = new Map();
+// staticReply['ping'] = 'Pong!';
+// staticReply['hi'] = 'Hi!';
+// staticReply['hello'] = 'Hello!';
+// staticReply['lefty help'] = '';
+// //console.log(staticReply);
 
-let dynamicReply = new Map();
-dynamicReply['roll'] = roller;
+// let dynamicReply = new Map();
+// dynamicReply['roll'] = roller;
 
-let searchWords = new Map();
-
-
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-
-});
+// let searchWords = new Map();
 
 
-client.on('message', message => {
-    if (message.author.bot) return;
-    console.log(message.author.username + ': ' + message.content);
-    for (const [key, value] of Object.entries(staticReply)) {
-        if (message.content.startsWith(key)) {
-            message.channel.send(value);
-        }
-    }
-    for (const [key, value] of Object.entries(dynamicReply)) {
-        if (message.content.startsWith(key)) {
-            message.channel.send(value(message));
-        }
-    }
-});
+// client.on('interactionCreate', async (interaction) => {
+// 	if (!interaction.isCommand()) return;
+//     const command = client.commands.get(interaction.commandName);
+// 	if (!command) return;
+// 	try {
+// 		await command.execute(interaction);
+// 	} catch (error) {
+// 		console.error(error);
+// 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 	}
+// });
+
+
+// client.on('messageCreate', message => {
+//     console.log(message.author.username + ': ' + message.content);
+//     if (message.author.bot) return;
+//     for (const [key, value] of Object.entries(staticReply)) {
+//         if (message.content.startsWith(key)) {
+//             message.channel.send(value);
+//         }
+//     }
+//     for (const [key, value] of Object.entries(dynamicReply)) {
+//         if (message.content.startsWith(key)) {
+//             message.channel.send(value(message));
+//         }
+//     }
+// });
+
+// console.log(client);
 
 client.login(token);
 
--- deploy_commands.js
+++ deploy_commands.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
@@ -14,6 +13,17 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
